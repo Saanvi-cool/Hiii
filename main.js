17a4da107f28d9d994a3bac2e82f70d767cbdb83
Saanvi-cool <84757010+Saanvi-cool@users.noreply.github.com>
2021-10-02 20:27:04 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach("#camera");
+function take_snapshot(){
+Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML='<img id+"captured_image" src="'+data_uri+'"/>'
+
+});
+}
+console.log("ml5 version is",ml5.version);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Zx4I4hn09/model.json ",ModelLoaded());
+function ModelLoaded(){
+    console.log("Model loaded succesfully!");
+}
+function check(){
+    img=document.getElementById("captured_image");
+    classifier.classify(img,getResult);
+}
+function getResult(error,result){
+if (error){
+    console.error(error);
+    
+}
+else{
+    console.log(result);
+    document.getElementById("Name").innerHTML=result[0].label;
+    document.getElementById("Percentage").innerHTML=result[0].confidence.toFixed(3);
+
+}
+}
+
